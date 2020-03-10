07c3b04f10a8d3dd14f61babf6d189518b837b57
erinboberin <erin.croteau@gmail.com>
2020-03-10 17:09:43 -0400

    EAC started working on Pizza Service

--- POS.Services/PizzaService.cs
+++ POS.Services/PizzaService.cs
@@ -33,7 +33,7 @@ namespace POS.Services
                     Cheese= model.Cheese,
                     Comment = model.Comment,
                     TypeOfCrust = model.TypeOfCrust,
-                    TypeOfSauce = model.TypeOfSauce,
+                    TypeOfSauce = model.TypeOfSauce, //EAC just changed OF to of in model.TypeOfSauce
                     TypeOfSize = model.TypeOfSize,
                     TypeOfToppingOne = model.TypeOfToppingOne,
                     TypeOfToppingTwo = model.TypeOfToppingTwo,
@@ -50,7 +50,7 @@ namespace POS.Services
         }
         public IEnumerable<PizzaListItem> GetPizzas()
         {
-            using (var ctx = new ApplicationDbContext())
+            using (var ctx = new ApplicationDbContext()) //EAC: why redunderlining of ctx here?
             {
                 var query =
                     ctx
@@ -69,6 +69,38 @@ namespace POS.Services
 
                 return query.ToArray();
             }
+            //var ctx = new ApplicationDbContext() //EAC: these three lines are showing errors
+            //    var x = ctx.Orders[0];
+            //var y = x.PizzaCollection[0];
+
         }
+
+        //Get Pizza by Id - EAC -- EAC stopped here 5pm, switch from ElevenNote template to Pizza stuff
+        //public NoteDetail GetNoteById(int id)
+        //{
+        //    using (var ctx = new ApplicationDbContext())
+        //    {
+        //        var entity =
+        //            ctx
+        //                .Notes
+        //                .Single(e => e.NoteId == id && e.OwnerId == _userId);
+        //        return
+        //            new NoteDetail
+        //            {
+        //                NoteId = entity.NoteId,
+        //                Title = entity.Title,
+        //                Content = entity.Content,
+        //                CreatedUtc = entity.CreatedUtc,
+        //                ModifiedUtc = entity.ModifiedUtc
+        //            };
+        //    }
+        }
+
+        //Update/Edit Pizzas -EAC
+
+
+
+
+        //Delete Pizzas -EAC
     }
 }
