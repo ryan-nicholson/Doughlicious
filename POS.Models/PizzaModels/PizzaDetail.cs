7a536bd95265513b5f2c6d4c5105666c40c7fc28
erinboberin <erin.croteau@gmail.com>
2020-03-11 11:17:22 -0400

    Merging most user role changes from FeaturedUserBranch with ErinBranch morning 3-11

---
+++ POS.Models/PizzaModels/PizzaDetail.cs
@@ -0,0 +1,42 @@
+﻿using POS.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static POS.Data.Pizza;
+
+namespace POS.Models.PizzaModels
+{
+    public class PizzaDetail
+    {
+        public int PizzaId { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+
+        public string EmployeeId { get; set; }//EAC: do we want/need EmployeeId and CustomerId in this or any pizza models?
+        public int CustomerId { get; set; }
+
+        public bool Cheese { get; set; }
+
+        public CrustType TypeOfCrust { get; set; }
+
+        public SauceType TypeOfSauce { get; set; }
+
+        public SizeType TypeOfSize { get; set; }
+
+        public ToppingType? TypeOfToppingOne { get; set; }
+
+        public ToppingType? TypeOfToppingTwo { get; set; }
+
+        public ToppingType? TypeOfToppingThree { get; set; }
+
+        public ToppingType? TypeOfToppingFour { get; set; }
+
+        public ToppingType? TypeOfToppingFive { get; set; }
+
+        public string Comment { get; set; }//We need to set default value to ""
+
+    }
+}
---
+++ POS.Models/PizzaModels/PizzaEdit.cs
@@ -0,0 +1,41 @@
+﻿using POS.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static POS.Data.Pizza;
+
+namespace POS.Models.PizzaModels
+{
+    public class PizzaEdit
+    {
+        public int PizzaId { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+
+        public string EmployeeId { get; set; }//EAC: do we want to be able to edit EmployeeId in a pizza? I think probably not
+        public int CustomerId { get; set; }//EAC: do we want to be able to edit CustomerId in a pizza? I think probably not
+
+        public bool Cheese { get; set; }
+
+        public CrustType TypeOfCrust { get; set; }
+
+        public SauceType TypeOfSauce { get; set; }
+
+        public SizeType TypeOfSize { get; set; }
+
+        public ToppingType? TypeOfToppingOne { get; set; }
+
+        public ToppingType? TypeOfToppingTwo { get; set; }
+
+        public ToppingType? TypeOfToppingThree { get; set; }
+
+        public ToppingType? TypeOfToppingFour { get; set; }
+
+        public ToppingType? TypeOfToppingFive { get; set; }
+
+        public string Comment { get; set; }//We need to set default value to ""
+    }
+}
