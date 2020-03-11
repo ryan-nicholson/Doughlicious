e1528e0822756b7ba7894cc21723203d8043d873
Arthur Robinson <arthurrobinsoniii3@gmail.com>
2020-03-11 16:33:34 -0400

    Integrated SeanTestBranch

--- POS.Models/OrderModels/OrderEdit.cs
+++ POS.Models/OrderModels/OrderEdit.cs
@@ -9,7 +9,7 @@ namespace POS.Models.OrderModels
 {
     public class OrderEdit
     {
-        //public int OrderId { get; set; }
+        public int OrderId { get; set; }
 
         public ICollection<Pizza> Pizzas { get; set; }
 
