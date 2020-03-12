1b5c7aa002b8d34876ea10386f25a21356bf8d7f
Arthur Robinson <arthurrobinsoniii3@gmail.com>
2020-03-12 16:49:38 -0400

    shenanigans

--- POS.Services/UserService.cs
+++ POS.Services/UserService.cs
@@ -73,7 +73,7 @@ namespace POS.Services
                 var user = ctx.UserTable.Single(e => e.Email == email);
 
 
-                return Guid.Parse(user.Id);
+                return user.UserGuid;
             }
         }
         public UserListItem GetUserByGuid(string email)
